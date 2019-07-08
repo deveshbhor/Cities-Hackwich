170bf1e9de0cca14b82bd69461837fd2eb2c524f
Devesh Bhor <deveshbhor@outlook.com>
2019-07-08 14:07:43 -0500

    Connected UI Objects to Storyboard

--- Cities Hackwich/DetailViewController.swift
+++ Cities Hackwich/DetailViewController.swift
@@ -10,30 +10,47 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var detailItem: City? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+    
 
     func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+        //update the user interface for the detail item
+        if let city = self.detailItem {
+            if cityTextField != nil {
+                cityTextField.text = city.name
+                stateTextField.text = city.name
+                populationTextField.text = String(city.population)
+                imageView.image = UIImage(data: city.image)
             }
         }
     }
 
+    func viewWillDisappear() {
+        if let city = self.detailItem {
+            city.name = cityTextField.text!
+            city.state = stateTextField.text!
+            city.population = Int(populationTextField.text!)!
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         configureView()
     }
 
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
-    }
+   
 
 
 }
